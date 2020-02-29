b3300e0c495da3f72693c95d791962f9f6412e2a
jojo <jojo@ulewu.de>
2020-02-29 14:20:45 +0100

    Bearbeiter-GUI Anpassung
    Felder in Table gesetzt

--- client/src/components/personal/personalShowPers.vue
+++ client/src/components/personal/personalShowPers.vue
@@ -2,7 +2,7 @@
 
     <div class="entryRow form-group">
         <h3>------ Status - Personal --- ----</h3>
-        <table  border="1" class="table  table-striped table-dark">
+        <table border="1" class="table  table-striped table-dark">
             <thead class="table table-dark">
                 <tr>
                     <th>Firma</th>
@@ -21,7 +21,7 @@
                 </tr>
             </thead>
             <tbody>
-                 <tr v-for="per in this.Personal" v-bind:key="per.id">
+                <tr v-for="per in this.Personal" v-bind:key="per.id">
                     <td>{{per.firma}}</td>
                     <td>{{per.persnr}}</td>
                     <td>{{per.nachname}}</td>
@@ -29,15 +29,25 @@
                     <td>{{per.titel}}</td>
                     <td>{{per.geschlecht}}</td>
                     <td>{{per.eintritt}}</td>
-                    <td>{{per.austritt}}</td>                    
-                    <td v-if="per.abteilungStatus == 1">{{per.abteilung}} - <span class="badge badge-success">OK</span></td>
-                    <td v-else>{{per.abteilung}} - <span class="badge badge-warning">Offen</span></td>
+                    <td>{{per.austritt}}</td>
+                    <td v-if="per.abteilungStatus == 1">{{per.abteilung}}
+                        -
+                        <span class="badge badge-success">OK</span>
+                    </td>
+                    <td v-else="v-else">{{per.abteilung}}
+                        -
+                        <span class="badge badge-warning">Offen</span>
+                    </td>
                     <td>{{per.gebdat}}</td>
                     <td>{{per.dienstart}}</td>
-                    <td>{{per.berufsbezeichnung}}</td>                    
-                    <td v-if="per.processorStatus == 1"><span class="badge badge-success">OK</span></td>
-                    <td v-else><span class="badge badge-warning">Offen</span></td>
-                 </tr>
+                    <td>{{per.berufsbezeichnung}}</td>
+                    <td v-if="per.processorStatus == 1">
+                        <span class="badge badge-success">OK</span>
+                    </td>
+                    <td v-else="v-else">
+                        <span class="badge badge-warning">Offen</span>
+                    </td>
+                </tr>
             </tbody>
         </table>
 
@@ -46,38 +56,31 @@
 </template>
 <script>
 
-import DBService from '../DBService';
+    import DBService from '../DBService';
 
-export default {
-    name: 'personalShowPers',
-    components: {    
-    },
-    data() {
-        return {
-            Personal: [],
-        }
-    },
-    created () {
-        this.updateAll()
-    },
-    methods: {
-       async updateAll() {
-  /*           await DBService
+    export default {
+        name: 'personalShowPers', 
+        components: {},
+        data() {
+            return {Personal: []}
+        },
+        created() {
+            this.updateAll()
+        },
+        methods: {
+            async updateAll() {
+                /*           await DBService
             // 'Range' wird bei Personal-Get nicht benötigt.
-            // wurde aber der Einfachheit halber gelassen, da sonnst in der App ein 
+            // wurde aber der Einfachheit halber gelassen, da sonnst in der App ein
             // extra DBGet gemacht werden müsste
             .DBget("Personal","All")
             .then((_personal) => {
                 this.Personal = _personal
             })
             .catch((error) => alert(error)); */
+            }
         }
     }
-}
 </script>
 
-
-<style>
-
-</style>
-
+<style></style>
